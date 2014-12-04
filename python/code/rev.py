c1f58b7879c0acca9fa54f3c403e99d980bad802
Arno Proeme <arno.proeme@ed.ac.uk>
2014-12-04 09:19:20 +0000

    Added further documentation

--- python/code/rev.py
+++ python/code/rev.py
@@ -1,3 +1,11 @@
+#
+# This defines a function that returns the reverse of a string, 
+# using a loop
+#
+#
+#
+
+
 def rev(string):
     reverse_string = ''
     for c in range(len(string)-1, -1, -1):
